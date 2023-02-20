a63ec6eb914aeaaa42bf1a13a5a9d3beaa77710a
Mihaela Man <mihaela_universo@yahoo.es>
2023-02-20 15:31:06 +0200

    Initial project

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello World")  # this is an inline comment
+# no ; for syntax
+#  ctrl D copies the line
+# int nr fara virgule float nr cu zecimale + complex
+#  print(1, type(1))
+#  print(2.5, type(2.5))
+#  print(2.5+5j, type(2.5+5j))
+#  print(1, type(1), float(1),complex(1))
+#  print(2.5, type(2.5), int(2.5),complex(2.5))
+#  print(2.5+5j, type(2.5+5j), float(2.5+5j)) nr complexe nu se pot transf in int sau float
+
+#  OPERATORI modulo %=restul impartirii si // = catul impartirii si ** = ridicare la putere
+print(7 / 4, 7 // 4, 7 % 4, 2 ** 4)
+
+#  OPERATORI Boolean in python se scriu cu T si F majuscule (True, False)

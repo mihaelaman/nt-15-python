f14a9bbba1d579a924f9d86fca1da021b3604183
Mihaela Man <mihaela_universo@yahoo.es>
2023-02-25 16:16:40 +0200

    Curs 2

---
+++ python_basics/python_basics.py
@@ -0,0 +1,23 @@
+# varsta_copil_mic = 10
+# varsta_copil_mare = 12
+
+# print(varsta_copil_mic, varsta_copil_mare)
+# print("copil mic = ", varsta_copil_mic)
+# print("copil mare = ", varsta_copil_mare)
+
+# impartirea cu operatia modulo adica '%' la 2 aflam daca nr este PAR sau IMPAR
+
+# insirarea operatorilor se face in ordinea op
+
+# functia id() este referinta catre locatia de memorie a variabilei
+
+a = 3
+print('a = ', a)
+a = a + 5  # overrides the first value of a at another id location or the same one as it evaluates the memory save
+
+# None = NULL in PYTHON
+
+# sir de caract = STRING def intre ghilimele duble (preferabil)
+
+# backslash \ este un 'escape' adica un caracter nu are validatatea din mod normal
+

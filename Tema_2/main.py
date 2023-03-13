e255e3552c3b615fb74bcd58761b6b18766d575c
Mihaela Man <mihaela_universo@yahoo.es>
2023-03-13 17:59:06 +0200

    revised homework

--- Tema_2/main.py
+++ Tema_2/main.py
@@ -14,19 +14,19 @@
 
 # scriem codul de mai sus in care utilizatorul introduce date de maxim 3 ori
 
-for i in range(3):
-    try:
-        age = int(input('Age: '))
-    except ValueError:
-        print("Invalid valid. Try again.")
-        continue
-
-    if age >= 18:
-        print("You are a grown up!")
-    else:
-        print("You are a child!")
-
-    break
+# for i in range(3):
+#     try:
+#         age = int(input('Age: '))
+#     except ValueError:
+#         print("Invalid valid. Try again.")
+#         continue
+#
+#     if age >= 18:
+#         print("You are a grown up!")
+#     else:
+#         print("You are a child!")
+#
+#     break
 
 # Să se scrie o funcție care primește un număr nedefinit de parametrii și să se calculeze suma parametrilor
 # care reprezintă numere întregi sau reale.
@@ -35,69 +35,55 @@ for i in range(3):
 #       your_function(2, 4, ‘abc’, param_1=2) va returna 6 (2 + 4).
 
 
-def your_function(*args):
+def your_function(*args, **kwargs):
+    sum = 0
 
-        sum = 0
-        operations = ""
+    if len(args) == 0 and len(kwargs) == 0:
+        return 0
 
-        if len(args) == 0:
-            return 0
+    for arg in args:
+        if isinstance(arg, (int, float)):
+            sum = sum + arg
 
-        for arg in args:
-            if isinstance(arg, (int, float)):
-                sum = sum + arg
-                if arg < 0:
-                    operations = operations + str(arg)
-                else:
-                    operations = operations + "+" + str(arg)
-        operations = operations[1:]
-        return str(sum) + f" ({operations})"
+    if kwargs:
+        for kwarg in kwargs:
+            if isinstance(kwarg, (int, float)):
+                sum = sum + kwarg
+
+    return str(sum)
+
 
 your_function()
 print(your_function())
 a = your_function(1, 5, -3, 'abc', [12, 56, 'cad'])
 print(a)
-# a = your_function(2, 4, 'abc', param_1=2)
-# print(a)
+a = your_function(2, 4, 'abc', param_1=2)
+print(a)
+
 
 # Să se scrie o funcție recursivă care primește ca parametru un număr întreg și returnează:
 #   suma tuturor numerelor de la [0, n]
-
-
-def sum_total (n):
-    if n==0:
-        return 0
-    else:
-        return n + sum_total(n-1)
-
-print("sum = ", sum_total(9))
-
-
 #   suma numerelor pare de la [0, n]
-def sum_par (n):
-    if n == 0:
-        return 0
-    if n%2==0:
-        return n + sum_par(n-1)
-    else:
-        return sum_par(n-1)
-
-
-print("sum = ", sum_par(4))
-
 #   suma numerelor impare de la [0, n]
-def sum_impar (n):
+
+
+def suma(n):
     if n == 0:
-        return 0
-    if n%2==0:
-        return sum_impar(n-1)
+        return 0, 0, 0
     else:
-        return n + sum_impar(n-1)
+        sum_par, suma_total, sum_impar = suma(n - 1)
+        suma_total += n
+        if n % 2 == 0:
+            sum_par += n
+        else:
+            sum_impar += n
+    return suma_total, sum_par, sum_impar
 
 
-print("sum = ", sum_impar(8))
+print(suma(3))
 
-#Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta este un număr întreg,
+
+# Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta este un număr întreg,
 # altfel returnează valoarea 0
 
 def verify_int():
@@ -106,4 +92,6 @@ def verify_int():
         return a
     except ValueError:
         return 0
+
+
 print(verify_int())

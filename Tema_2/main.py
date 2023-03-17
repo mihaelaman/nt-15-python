3fc8c952b8ac4ac161efc091766f05293faf7d0a
Mihaela Man <mihaela_universo@yahoo.es>
2023-03-17 17:15:16 +0200

    revised homework

--- Tema_2/main.py
+++ Tema_2/main.py
@@ -34,32 +34,35 @@
 #       your_function() va returna 0.
 #       your_function(2, 4, ‘abc’, param_1=2) va returna 6 (2 + 4).
 
-
-def your_function(*args, **kwargs):
-    sum = 0
+#  #1: Account for kwargs and sum all numbers in there
+#  #2: Account for every number at any level in both args and kwargs
+def total_sum(*args, **kwargs):
+    total = 0
 
     if len(args) == 0 and len(kwargs) == 0:
         return 0
 
     for arg in args:
         if isinstance(arg, (int, float)):
-            sum = sum + arg
+            total = total + arg
+        if isinstance(arg, str) and arg.isdigit():
+            total = total + int(arg)
+        if isinstance(arg, list) or isinstance(arg, tuple):
+            total = total + total_sum(*arg)
 
-    if kwargs:
-        for kwarg in kwargs:
-            if isinstance(kwarg, (int, float)):
-                sum = sum + kwarg
+    for kwarg in kwargs.values():
+        if isinstance(kwarg, (int, float)):
+            total = total + kwarg
 
-    return str(sum)
+    return total
 
 
-your_function()
-print(your_function())
-a = your_function(1, 5, -3, 'abc', [12, 56, 'cad'])
+print(total_sum())
+a = total_sum(1, 5, -3, 'abc', [12, 56, 'cad'])
 print(a)
-a = your_function(2, 4, 'abc', param_1=2)
+a = total_sum(2, 4, 'abc', param_1=2, param_3=10)
 print(a)
-
+print(total_sum(1, 5, -3, "abc", "12", [12, 56, [1, 2], "cad"]))
 
 # Să se scrie o funcție recursivă care primește ca parametru un număr întreg și returnează:
 #   suma tuturor numerelor de la [0, n]
@@ -67,31 +70,31 @@ print(a)
 #   suma numerelor impare de la [0, n]
 
 
-def suma(n):
-    if n == 0:
-        return 0, 0, 0
-    else:
-        sum_par, suma_total, sum_impar = suma(n - 1)
-        suma_total += n
-        if n % 2 == 0:
-            sum_par += n
-        else:
-            sum_impar += n
-    return suma_total, sum_par, sum_impar
-
-
-print(suma(3))
+# def suma(n):
+#     if n == 0:
+#         return 0, 0, 0
+#     else:
+#         sum_par, suma_total, sum_impar = suma(n - 1)
+#         suma_total += n
+#         if n % 2 == 0:
+#             sum_par += n
+#         else:
+#             sum_impar += n
+#     return suma_total, sum_par, sum_impar
+#
+#
+# print(suma(3))
 
 
 # Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta este un număr întreg,
 # altfel returnează valoarea 0
 
-def verify_int():
-    try:
-        a = int(input('number: '))
-        return a
-    except ValueError:
-        return 0
-
-
-print(verify_int())
+# def verify_int():
+#     try:
+#         a = int(input('number: '))
+#         return a
+#     except ValueError:
+#         return 0
+#
+#
+# print(verify_int())
